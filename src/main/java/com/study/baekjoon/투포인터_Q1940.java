f8e3d4f26cef73674335ae32f4bf6910b40697e8
jeoningu <doremipasolsol1@gmail.com>
2023-11-09 23:49:27 +0900

    백준 - 슬라이딩_Q12891  코드, 설명 정리

--- src/main/java/com/study/baekjoon/투포인터_Q1940.java
+++ src/main/java/com/study/baekjoon/투포인터_Q1940.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class 투포인터_Q1940 {
